cc486a3b0a468793d1bde4d5e830661381ea8bc6
Jeevan <jpil8@student.monash.edu>
2019-06-05 15:25:50 +1000

    Added user schema

--- server/server.js
+++ server/server.js
@@ -4,12 +4,17 @@ require('dotenv').config({
     path: 'variables.env'
 });
 
+// import schema
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
+
+// connect to database
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => console.log('DB Connected'))
     .catch(err => console.error(err));
 
-
+// initialise application
 const app = express();
 const PORT = process.env.PORT || 4444;
 
