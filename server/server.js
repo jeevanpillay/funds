67d15c042a9bc84ebd3649a3e96ce71ec783aec4
lpfloyd <jeevan.pillay8@gmail.com>
2019-06-06 15:39:35 +1000

    Added database connections

--- server/server.js
+++ server/server.js
@@ -1,55 +1,64 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { ApolloServer } = require('apollo-server-express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
+// Imports
+const express = require("express");
+const mongoose = require("mongoose");
+const { ApolloServer } = require("apollo-server-express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const chalk = require("chalk");
+
+// Configure chalk
+const error = chalk.bold.red;
+const success = chalk.bold.green;
+const connection = chalk.bold.magenta;
 
 // Configure PORT
-require('dotenv').config({
-    path: 'variables.env'
+require("dotenv").config({
+  path: "variables.env"
 });
 const PORT = process.env.PORT || 4444;
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 // Mongoose Models
-const Recipe = require('./models/Recipe');
-const User = require('./models/User');
+const Recipe = require("./models/Recipe");
+const User = require("./models/User");
 
 // Create Schemas
-const { typeDefs } = require('./schema');
-const { resolvers } = require('./resolvers');
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
 
 // connect to database
 mongoose
-  .connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true }
-  )
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => {
-      console.log("DB connected");
-      mongoose.set('useCreateIndex', true);
-    })
-  .catch(err => console.error(err));
+    console.log(
+      success(
+        `Connected to database:\n->>> ${connection(process.env.MONGO_URI)}`
+      )
+    );
+  })
+  .catch(err => console.log(error(err)));
 
-  // initialise application
+mongoose.set("useCreateIndex", true);
+
+// initialise application
 const app = express();
 
 // Setup cors so that client can talk to back end
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true
+  origin: "http://localhost:3000",
+  credentials: true
 };
 app.use(cors(corsOptions));
 
 // JWT authentication middleware
 app.use(async (req, res, next) => {
   const token = req.headers["authorization"];
-  if (token !== "null" && token !== undefined) {
+  if (token !== "null" && token !== undefined && token !== null) {
     try {
-        const currentUser = await jwt.verify(token, process.env.SECRET);
-        req.currentUser = currentUser;
-    } 
-    catch (err) {
-        console.log(error(err));
+      const currentUser = await jwt.verify(token, process.env.SECRET);
+      req.currentUser = currentUser;
+    } catch (err) {
+      console.log(error(err));
     }
   }
   next();
@@ -57,14 +66,20 @@ app.use(async (req, res, next) => {
 
 // Initialise ApolloServer with the associated typeDefs and resolvers.
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req }) => ({ Recipe, User, currentUser: req.currentUser }),
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ Recipe, User, currentUser: req.currentUser })
 });
-const path = '/graphql';
-server.applyMiddleware({ app, path })
+const path = "/graphql";
+server.applyMiddleware({ app, path });
 
-// Listen to PORT specified
+// Listen to the Port
 app.listen(PORT, () => {
-    console.log(`Server listen on PORT ${PORT}`)
+  console.log(
+    success(
+      `Server listening on ${chalk.blue("PORT " + PORT)}: ${connection(
+        NODE_ENV
+      )}`
+    )
+  );
 });
