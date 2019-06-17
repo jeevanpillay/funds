7c03547d2a0b7dcf971033431316ce45a76071d4
lpfloyd <jeevan.pillay8@gmail.com>
2019-06-17 14:37:56 +1000

    Refactor resolver export format

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,9 +2,9 @@
 const merge = require('lodash').merge;
 
 // Resolvers
-const UserResolvers = require('./models/user/user.resolvers').resolvers;
-const WithdrawalResolvers = require('./models/user/token/withdrawal/withdrawal.resolvers').resolvers;
-const DepositResolvers = require('./models/user/token/deposit/deposit.resolvers').resolvers;
+const user = require('./models/user/user.resolvers').resolvers;
+const withdrawal = require('./models/user/token/withdrawal/withdrawal.resolvers').resolvers;
+const deposit = require('./models/user/token/deposit/deposit.resolvers').resolvers;
 
 // Export
-module.exports = merge(UserResolvers, WithdrawalResolvers, DepositResolvers);
+module.exports = merge(user, withdrawal, deposit);
