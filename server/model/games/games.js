616dd5c9d88b2a70af5fe201f0322aa1f404b3ac
lpfloyd <jeevan.pillay8@gmail.com>
2019-06-19 15:37:12 +1000

    Refactored games schema to be empty

--- server/model/games/games.js
+++ server/model/games/games.js
@@ -1,46 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const {
-  amountValidator,
-  multiplierValidator
-} = require("./bet.validator");
 
-const BetSchema = new Schema({
-  amount: {
-    type: Number,
-    required: true,
-    validate: amountValidator
-  },
-  time: {
-    type: Date,
-    default: Date.now(),
-  },
-  bonus: {
-    type: Number,
-    default: 0,
-    validate: multiplierValidator
-  },
-  multiplier: {
-    type: Number,
-    required: true,
-    validate: multiplierValidator,
-    alias: "cashOut"
-  },
-  status: {
-    type: Boolean,
-    default: false,
-    alias: "hasEnded"
-  },
-  address: {
-    type: String,
-    required: true,
-    alias: "creatorAddress"
-  },
-  hash: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "GamesHash"
-  }
+const GamesSchema = new Schema({
+
 });
 
-module.exports = mongoose.model("Bet", BetSchema);
+module.exports = mongoose.model("Games", GamesSchema);
--- server/model/games/games.test.js
+++ server/model/games/games.test.js
@@ -4,13 +4,13 @@ const assert = require("chai").assert;
 const mongoose = require("mongoose");
 
 // model
-const Bet = require("./bet");
+const Games = require("./games");
 
 // test
-describe("Bet schema", function () {
+describe("Games schema", function () {
     describe("amount", () => {
         it("is invalid if empty", function (done) {
-            var g = new Bet();
+            var g = new Games();
 
             g.validate(function (err) {
                 expect(err.errors.amount).to.exist;
@@ -20,7 +20,7 @@ describe("Bet schema", function () {
 
         it("should be more than 0", function (done) {
             // test lower boundary
-            var g = new Bet({
+            var g = new Games({
                 amount: -1
             });
 
@@ -29,7 +29,7 @@ describe("Bet schema", function () {
             });
 
             // test
-            var g = new Bet({
+            var g = new Games({
                 amount: 0
             });
             g.validate(function (err) {
@@ -37,7 +37,7 @@ describe("Bet schema", function () {
             });
 
             // test upper boundary
-            var g = new Bet({
+            var g = new Games({
                 amount: 1
             });
             g.validate(function (err) {
@@ -50,7 +50,7 @@ describe("Bet schema", function () {
 
     describe("Time", () => {
         it("valid if empty", function (done) {
-            var g = new Bet();
+            var g = new Games();
 
             g.validate(function (err) {
                 expect(err.errors.time).to.not.exist;
@@ -59,7 +59,7 @@ describe("Bet schema", function () {
         });
 
         it("must be equal to the current date", function (done) {
-            var g = new Bet();
+            var g = new Games();
 
             assert.equal(g.time.Date, Date.now().Date);
             done();
@@ -68,7 +68,7 @@ describe("Bet schema", function () {
 
     describe("Bonus", function () {
         it("is valid if empty", function (done) {
-            var g = new Bet();
+            var g = new Games();
 
             g.validate(function (err) {
                 expect(err.errors.bonus).to.not.exist;
@@ -78,7 +78,7 @@ describe("Bet schema", function () {
 
         it("should be more than 0", function (done) {
             // test lower boundary
-            var g = new Bet({
+            var g = new Games({
                 bonus: -1
             });
 
@@ -87,7 +87,7 @@ describe("Bet schema", function () {
             });
 
             // test
-            var g = new Bet({
+            var g = new Games({
                 bonus: 0
             });
             g.validate(function (err) {
@@ -95,7 +95,7 @@ describe("Bet schema", function () {
             });
 
             // test upper boundary
-            var g = new Bet({
+            var g = new Games({
                 bonus: 1
             });
             g.validate(function (err) {
@@ -106,7 +106,7 @@ describe("Bet schema", function () {
         });
 
         it("should be able to have decimal values", function (done) {
-            var g = new Bet({
+            var g = new Games({
                 bonus: 1.1
             });
 
@@ -119,7 +119,7 @@ describe("Bet schema", function () {
 
     describe("Multiplier", function () {
         it("is invalid if empty", function (done) {
-            var g = new Bet();
+            var g = new Games();
 
             g.validate(function (err) {
                 expect(err.errors.multiplier).to.exist;
@@ -129,7 +129,7 @@ describe("Bet schema", function () {
 
         it("should be more than or equal to 0", function (done) {
             // test lower boundary
-            var g = new Bet({
+            var g = new Games({
                 multiplier: -1
             });
 
@@ -138,7 +138,7 @@ describe("Bet schema", function () {
             });
 
             // test
-            var g = new Bet({
+            var g = new Games({
                 multiplier: 0
             });
             g.validate(function (err) {
@@ -146,7 +146,7 @@ describe("Bet schema", function () {
             });
 
             // test upper boundary
-            var g = new Bet({
+            var g = new Games({
                 multiplier: 1
             });
             g.validate(function (err) {
@@ -157,7 +157,7 @@ describe("Bet schema", function () {
         });
 
         it("should be able to have decimal values", function (done) {
-            var g = new Bet({
+            var g = new Games({
                 multiplier: 1.1
             });
 
@@ -168,7 +168,7 @@ describe("Bet schema", function () {
         });
 
         it("should can have very large values", function (done) {
-            var g = new Bet({
+            var g = new Games({
                 multiplier: 1000
             });
 
@@ -176,7 +176,7 @@ describe("Bet schema", function () {
                 expect(err.errors.multiplier).to.not.exist;
             });
 
-            var g = new Bet({
+            var g = new Games({
                 multiplier: 10000
             });
 
@@ -184,7 +184,7 @@ describe("Bet schema", function () {
                 expect(err.errors.multiplier).to.not.exist;
             });
 
-            var g = new Bet({
+            var g = new Games({
                 multiplier: 111.11
             });
 
@@ -196,9 +196,9 @@ describe("Bet schema", function () {
         });
     });
 
-    describe("BetsHash", function () {
+    describe("GamessHash", function () {
         it("is invalid if empty", function (done) {
-            let g = new Bet();
+            let g = new Games();
 
             g.validate(function (err) {
                 expect(err.errors.hash).to.exist;
@@ -207,7 +207,7 @@ describe("Bet schema", function () {
         });
 
         it("is valid if type ObjectID is supplied", function (done) {
-            let g = new Bet({
+            let g = new Games({
                 hash: new mongoose.Types.ObjectId
             });
 
@@ -218,7 +218,7 @@ describe("Bet schema", function () {
         });
 
         it("is invalid if type ObjectID is not supplied", function (done) {
-            let g = new Bet({
+            let g = new Games({
                 hash: 1
             });
 
